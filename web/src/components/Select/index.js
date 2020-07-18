4635dc3bca5639664030a1d1e6d93d96a8f8b6c0
Lucas Lima <lucas.eng.lds@gmail.com>
2020-07-18 19:25:26 -0300

    [Web] End integration with the api

--- web/src/components/Select/index.js
+++ web/src/components/Select/index.js
@@ -3,9 +3,19 @@ import { uuid } from "uuidv4";
 
 import "./styles.css";
 
-function Select({ options, label }) {
+function Select({ options, label, placeholder, onChange, value }) {
   return (
-    <select id={label} className="select">
+    <select
+      id={label}
+      className="select"
+      onChange={onChange}
+      value={value}
+      defaultValue={"DEFAULT"}
+    >
+      <option disabled value="DEFAULT">
+        {placeholder}
+      </option>
+
       {options.map((option) => (
         <option key={uuid()} className="select-option" value={option}>
           {option}
--- web/src/pages/FaleMais/index.js
+++ web/src/pages/FaleMais/index.js
@@ -1,17 +1,71 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { FaPlus } from "react-icons/fa";
-import { FiPhoneForwarded } from "react-icons/fi";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import "./styles.css";
 
+import api from "../../services/api";
 import Header from "../../components/Header";
 import Select from "../../components/Select";
 
 import womanImg from "../../assets/woman.svg";
 
 function FaleMais() {
+  const [originDDD, setOriginDDD] = useState([]);
+  const [destinyDDD, setDestinyDDD] = useState([]);
+  const [selectedOrigin, setSelectedOrigin] = useState(undefined);
+  const [selectedDestiny, setSelectedDestiny] = useState(undefined);
+
+  const [callTime, setCallTime] = useState(null);
+  const [callPlan, setCallPlan] = useState(undefined);
+
+  const [valueWithPlan, setValueWithPlan] = useState([]);
+  const [valueWithoutPlan, setValueWithoutPlan] = useState([]);
+
+  const handleSelectedOrigin = useCallback(async (e) => {
+    setSelectedOrigin(e.target.value);
+    const response = await api.get(`/?originDDD=${e.target.value}`);
+    setDestinyDDD(response.data.originMatches);
+  }, []);
+
+  const handleSelectedDestiny = useCallback(async (e) => {
+    setSelectedDestiny(e.target.value);
+    const response = await api.get(`/?destinyDDD=${e.target.value}`);
+    setOriginDDD(response.data.destinyMatches);
+  }, []);
+
+  const handleConsultPrice = useCallback(async () => {
+    const response = await api.get(
+      `/consulta?originDDD=${selectedOrigin}&destinyDDD=${selectedDestiny}&callTime=${callTime}&callPlan=${callPlan}`
+    );
+
+    setValueWithPlan(response.data.valueWithPlan);
+    setValueWithoutPlan(response.data.valueWithoutPlan);
+  }, [callPlan, callTime, selectedDestiny, selectedOrigin]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await api.get(`/`);
+
+      setOriginDDD(response.data.origins);
+      setDestinyDDD(response.data.destinies);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (isNaN(callTime)) {
+      toast.error("Insira um valor válido em minutos");
+      return;
+    }
+    if (selectedOrigin && selectedDestiny && callTime && callPlan) {
+      handleConsultPrice();
+    }
+  }, [selectedOrigin, selectedDestiny, callTime, callPlan, handleConsultPrice]);
+
   return (
-    <>
+    <div>
       <Header />
       <main>
         <div className="img-container">
@@ -27,40 +81,47 @@ function FaleMais() {
           </div>
           <div className="inputs-container">
             <label htmlFor="originDDD">
-              <div className="input-title-container">
-                ORIGEM<span>(DDD)</span>
-              </div>
+              <div className="input-title-container">ORIGEM</div>
               <Select
-                options={[`0${11}`, `0${21}`, `0${31}`]}
+                options={originDDD}
                 label={"originDDD"}
+                placeholder="DDD"
+                onChange={handleSelectedOrigin}
+                value={selectedOrigin}
               />
             </label>
 
-            <FiPhoneForwarded size={14} />
+            <label htmlFor="call-time">
+              <div className="input-title-container">DURAÇÃO</div>
+              <input
+                id="call-time"
+                type="text"
+                placeholder="Minutos"
+                onChange={(e) => setCallTime(e.target.value)}
+              />
+            </label>
 
             <label htmlFor="destinyDDD">
-              <div className="input-title-container">
-                DESTINO<span>(DDD)</span>
-              </div>
+              <div className="input-title-container">DESTINO</div>
               <Select
-                options={[`0${11}`, `0${21}`, `0${31}`]}
-                label={"destinyDDD"}
+                options={destinyDDD}
+                label="destinyDDD"
+                placeholder="DDD"
+                onChange={handleSelectedDestiny}
+                value={selectedDestiny}
               />
             </label>
           </div>
 
           <div className="plan-input-container">
             <label htmlFor="call-plan">
-              <div className="input-title-container">
-                PLANO<span>Escolha uma opção</span>
-              </div>
+              <div className="input-title-container">PLANO FALEMAIS</div>
               <Select
-                options={[
-                  `Fale mais ${30}`,
-                  `Fale mais ${60}`,
-                  `Fale mais ${120}`,
-                ]}
-                label={"call-plan"}
+                options={[30, 60, 120]}
+                label="call-plan"
+                placeholder="Selecione seu plano"
+                onChange={(e) => setCallPlan(e.target.value)}
+                value={callPlan}
               />
             </label>
           </div>
@@ -70,8 +131,9 @@ function FaleMais() {
               <p>COM FALEMAIS</p>
               <div className="result-with-plan">
                 <span>R$</span>
-                <strong>{37}</strong>
-                <span>,{40}</span>
+                <strong>
+                  {(!!valueWithPlan.length && valueWithPlan) || "00,00"}
+                </strong>
               </div>
             </div>
             <div className="result-without-plan">
@@ -79,15 +141,17 @@ function FaleMais() {
                 <p>SEM FALEMAIS</p>
                 <div className="result-without-plan">
                   <span>R$</span>
-                  <strong>{136}</strong>
-                  <span>{`,${40}`}</span>
+                  <strong>
+                    {(!!valueWithoutPlan.length && valueWithoutPlan) || "00,00"}
+                  </strong>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <ToastContainer />
       </main>
-    </>
+    </div>
   );
 }
 
